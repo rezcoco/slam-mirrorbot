e01f7841f92040c2a99cff00d4c093adadb933b4
Rez <77873738+rezcoco@users.noreply.github.com>
2021-04-09 15:06:27 +0700

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -1,16 +1,13 @@
-from telegram.ext import CommandHandler, run_async
+from telegram.ext import CommandHandler
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.mirror_utils.download_utils.download_helper import DownloadHelper
-from bot.helper.ext_utils.bot_utils import get_readable_file_size
 from bot.helper.telegram_helper.message_utils import *
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.bot_utils import new_thread
-from bot import dispatcher, updater, bot
-import time
+from bot import dispatcher
 
 
-@run_async
+@new_thread
 def cloneNode(update,context):
     args = update.message.text.split(" ",maxsplit=1)
     if update.message.from_user.username:
@@ -21,48 +18,16 @@ def cloneNode(update,context):
             cc = f'\n\ncc: {uname}'
     if len(args) > 1:
         link = args[1]
-        try:
-            ignoreList = args[-1].split(',')
-        except IndexError:
-            ignoreList = []
-        
-        msg = sendMessage(f"<b>Cloning : </b> <code>{link}</code>", context.bot, update)
-        status_class = DownloadHelper()
+        msg = sendMessage(f"Cloning: <code>{link}</code>",context.bot,update)
         gd = GoogleDriveHelper()
-        sendCloneStatus(update, context, status_class, msg, link)
         result, button = gd.clone(link)
         deleteMessage(context.bot,msg)
-        status_class.set_status(True)
         if button == "":
             sendMessage(result,context.bot,update)
         else:
             sendMarkup(result + cc,context.bot,update,button)
     else:
         sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)
-    
-@run_async
-def sendCloneStatus(update, context, status, msg, link):
-    old_text = ''
-    while not status.done():
-        sleeper(3)
-        try:
-            text=f'🔗 *Cloning:* [{status.MainFolderName}]\n━━━━━━━━━━━━━━\n🗃️ *Current File:* `{status.get_name()}`\n⬆️ *Transferred*: `{status.get_size()}`\n📁 *Destination:* [{status.DestinationFolderName}]'
-            if status.checkFileStatus():
-                text += f"\n🕒 *Checking Existing Files:* `{str(status.checkFileStatus())}`"
-            if not text == old_text:
-                msg.edit_text(text=text, parse_mode="Markdown", timeout=200)
-                old_text = text
-        except Exception as e:
-            LOGGER.error(e)
-            if str(e) == "Message to edit not found":
-                break
-            sleeper(2)
-            continue
-        return
-    
-def sleeper(value, enabled=True):
-    time.sleep(int(value))
-    return
 
 clone_handler = CommandHandler(BotCommands.CloneCommand,cloneNode,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
 dispatcher.add_handler(clone_handler)
