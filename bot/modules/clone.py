ff0940fb8c062cac7bf52b79c054e0ca987ea18e
Rez <77873738+rezcoco@users.noreply.github.com>
2021-04-09 15:59:17 +0700

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -9,27 +9,25 @@ from bot import dispatcher
 
 @new_thread
 def cloneNode(update,context):
-    with download_dict_lock:
-        for download in list(download_dict.values()):
-            args = update.message.text.split(" ",maxsplit=1)
-            if update.message.from_user.username:
-                uname = f"@{update.message.from_user.username}"
-            else:
-                uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
-            if uname is not None:
-                    cc = f'\n\ncc: {uname}'
-            if len(args) > 1:
-                link = args[1]
-                msg = f"<b>📂 Filename : </b> <code>{download.name()}</code>"
-                gd = GoogleDriveHelper()
-                result, button = gd.clone(link)
-                deleteMessage(context.bot,msg)
-                if button == "":
-                    sendMessage(result,context.bot,update)
-                else:
-                    sendMarkup(result + cc,context.bot,update,button)
-            else:
-                sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)
+    args = update.message.text.split(" ",maxsplit=1)
+    if update.message.from_user.username:
+        uname = f"@{update.message.from_user.username}"
+    else:
+        uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+    if uname is not None:
+            cc = f'\n\ncc: {uname}'
+    if len(args) > 1:
+        link = args[1]
+        msg = sendMessage(f"Cloning: <code>{link}</code>",context.bot,update)
+        gd = GoogleDriveHelper()
+        result, button = gd.clone(link)
+        deleteMessage(context.bot,msg)
+        if button == "":
+            sendMessage(result,context.bot,update)
+        else:
+            sendMarkup(result + cc,context.bot,update,button)
+    else:
+        sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)
 
 clone_handler = CommandHandler(BotCommands.CloneCommand,cloneNode,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
 dispatcher.add_handler(clone_handler)
